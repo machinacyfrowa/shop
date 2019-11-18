d3722adc076daf24112720d1dac9d5b098b28ca9
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2019-11-18 13:12:33 +0100

    Number format and img location

--- class/Product.class.php
+++ class/Product.class.php
@@ -17,12 +17,12 @@ class Product {
     public function showProduct() {
         $buffer = '<div class="col-3 text-center">';
         $buffer .= '<h1>' . $this->name . '</h1>';
-        $buffer .= '<img src="' . $this->imageUrl . '" class="img-fluid">';
+        $buffer .= '<img src="img/' . $this->imageUrl . '" class="img-fluid">';
         if($this->discount != 0) {
-            $newPrice = $this->price * ( 1 - $this->discount );
+            $newPrice = number_format($this->price * ( 1 - $this->discount ) , 2);
             $buffer .= '<h3 style="color: red;">' . $newPrice . '</h3>';
         } else {
-            $buffer .= '<h3>' . $this->price . '</h3>';
+            $buffer .= '<h3>' . number_format($this->price , 2) . '</h3>';
         }
         $buffer .= '</div>';
         echo $buffer;
