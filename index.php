78eee3abfdcf69fcbc40be91e2a8ffd92fb90b5d
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2019-11-18 12:42:17 +0100

    Add test products to catalog

--- index.php
+++ index.php
@@ -12,7 +12,16 @@
   </head>
   <body>
     <?php
-    require_once('class/Catalog.class.php')
+    require_once('class/Catalog.class.php');
+    $c = new Catalog();
+    $p1 = new Product(1, "Produkt 1", 44.80, "product1.png", 0.2);
+    $p2 = new Product(2, "Produkt 2", 12.40, "product2.png", 0.3);
+    $p3 = new Product(3, "Produkt 3", 5.60, "product3.png", 0.23);
+    $p4 = new Product(4, "Produkt 4", 11.20, "product4.png", 0.5);
+    $c->addProduct($p1);
+    $c->addProduct($p2);
+    $c->addProduct($p3);
+    $c->addProduct($p4);
     ?>
 
     <!-- Optional JavaScript -->
