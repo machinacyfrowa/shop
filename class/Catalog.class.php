be1e88cf29194c26669d0d5816b67ed77982c4a7
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2020-02-03 12:15:08 +0100

    Move test data to catalog class

--- class/Catalog.class.php
+++ class/Catalog.class.php
@@ -8,6 +8,7 @@ class Catalog {
     public function __construct() {
         $this->products = Array();
         $this->categoryList = Array();
+        $this->loadTestData();
     }
 
     public function addProduct(Product $p) {
@@ -29,6 +30,20 @@ class Catalog {
         }
         $buffer .= "</ul>";
         echo $buffer;
+    }
+    public function loadTestData() {
+        $cat1 = new Category("Category 1");
+        $cat2 = new Category("Category 2");
+        $this->addCategory($cat1);
+        $this->addCategory($cat2);
+        $p1 = new Product(1, "Produkt 1", 44.80, "product1.png", 0.2);
+        $p2 = new Product(2, "Produkt 2", 12.40, "product2.png", 0.3);
+        $p3 = new Product(3, "Produkt 3", 5.60, "product3.png", 0.23);
+        $p4 = new Product(4, "Produkt 4", 11.20, "product4.png", 0.5);
+        $this->addProduct($p1);
+        $this->addProduct($p2);
+        $this->addProduct($p3);
+        $this->addProduct($p4);
     }
 }
 ?>
