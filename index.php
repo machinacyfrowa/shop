765a3bdfad7efbe0d9db390efeee96c1a537450b
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2020-02-13 12:10:44 +0100

    instalacja smarty

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php 
+require_once('smarty/Smarty.class.php');
+$smarty = new Smarty();
+
+$smarty->setTemplateDir('view');
+$smarty->setCompileDir('view/compiled');
+$smarty->setCacheDir('cache');
+$smarty->setConfigDir('config');
+
 require_once('class/Controller.class.php');
 $controller = new Controller();
 $controller->view();
